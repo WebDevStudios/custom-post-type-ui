e641ec4db7e279c456343524ce6955b291569393
Michael Beckwith <tw2113@gmail.com>
2013-08-17 17:49:09 -0500

    more ui class work to help make reusable code

--- classes/class.cptui_admin_ui.php
+++ classes/class.cptui_admin_ui.php
@@ -10,6 +10,31 @@ class ctpui_admin_ui {
 	}
 
 	/**
+	 * opening <tr> and <td> tags. All of <th> tag with values for label
+	 *
+	 * @since  0.9
+	 *
+	 * @param  string  $label_for  input name to associate with the wrapped input.
+	 * @param  string  $label_text label text to use with the input
+	 * @param  string  $title_text help text for the little question mark.
+	 *
+	 * @return string              completed opening wrapper
+	 */
+	public function tr_wrap_start( $label_for, $label_text, $title_text ) {
+		return '<tr valign="top"><th scope="row">' . $label_text . '<a href="#" title="' . $title_text . '" class="help">?</a></th><td>';
+	}
+
+	/**
+	 * closing <tr> and </td> tags
+	 *
+	 * @since  0.9
+	 *
+	 * @return string  completed closing wrapper
+	 */
+	public function tr_wrap_end() {
+		return '</td></tr>';
+	}
+	/**
 	 * Display a select input with true/false values.
 	 * @param  array  $args values to use in the input
 	 * @return string       constructed input for the form.
@@ -20,12 +45,15 @@ class ctpui_admin_ui {
 		);
 		$args = wp_parse_args( $args, $defaults );
 
-		$value = '<select name="' . $args['name'] . '" tabindex="' . $args['tabindex'] . '">';
+		$value = $this->tr_wrap_start('','');
+		$value .= '<select name="' . $args['name'] . '" tabindex="' . $args['tabindex'] . '">';
 
 		foreach( $args['values'] as $val ) {
 			$value .= '<option value="' . $val['value_int'] . '" ' . selected( $args['selected'], $val['value_int'] ) . '>' . $val['value_string'] . '</option>';
 		}
 		$value .= '</select>' . $args['help_text'];
+
+		$value .= $this->tr_wrap_end();
 
 		return $value;
 	}
@@ -41,6 +69,14 @@ class ctpui_admin_ui {
 		);
 		$args = wp_parse_args( $args, $defaults );
 
+		$value = $this->tr_wrap_start('','');
+
+		$value .= '<input type="text" name="" tabindex="" value="" /><br/>';
+
+		$value .= $this->tr_wrap_end();
+
+		return $value;
+
 	}
 
 	/**
@@ -54,6 +90,13 @@ class ctpui_admin_ui {
 		);
 		$args = wp_parse_args( $args, $defaults );
 
+		$value = $this->tr_wrap_start('','');
+
+		$value .= '<textarea name="" tabindex="" rows="" cols=""></textarea>';
+
+		$value .= $this->tr_wrap_end();
+
+		return $value;
 	}
 
 	/**
@@ -67,5 +110,6 @@ class ctpui_admin_ui {
 		);
 		$args = wp_parse_args( $args, $defaults );
 
+		$value = '<input type="checkbox" name="" tabindex="" value="" /> <a href="#" title="" class="help">?</a> <br/>';
 	}
 }
