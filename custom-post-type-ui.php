e94f729f0dd9027548f3b0a713ce46685ba9702c
Michael Beckwith <tw2113@gmail.com>
2013-11-30 22:28:42 -0600

    some extra css due to #excerpt having styling from elsewhere

--- custom-post-type-ui.php
+++ custom-post-type-ui.php
@@ -1967,6 +1967,7 @@ function cpt_help_style() { ?>
 			font-weight: bold;
 		}
 		.required { color: rgb(255,0,0); }
+		#slidepanel2 #excerpt { height: 16px; margin-right: 4px; width: auto; }
 	</style>
 <?php
 }
