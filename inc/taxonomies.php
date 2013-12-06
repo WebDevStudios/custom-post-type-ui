e727e226c2648048394914cf5ff5fe174278602c
Michael Beckwith <tw2113@gmail.com>
2013-12-06 01:08:06 -0600

    comments for future tab use

--- inc/taxonomies.php
+++ inc/taxonomies.php
@@ -1,2 +1,3 @@
 <?php
 add_submenu_page( 'cpt_main_menu', __( 'Add/Edit Taxonomies', 'cpt-plugin' ), __( 'Add/Edit Taxonomies', 'cpt-plugin' ), 'manage_options', 'cpt_sub_manage_taxonomies', 'cpt_manage_taxonomies' );
+//cpt_settings_tab_menu( $page = 'taxonomies' )
