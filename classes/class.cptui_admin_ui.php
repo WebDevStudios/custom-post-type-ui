a5cc3527a7b3a8f17c341ded8d4e5769a34fdaff
Michael Beckwith <tw2113@gmail.com>
2013-12-07 21:59:01 -0600

    documentation!

--- classes/class.cptui_admin_ui.php
+++ classes/class.cptui_admin_ui.php
@@ -9,52 +9,133 @@ class cptui_admin_ui {
 
 	}
 
+	/**
+	 * Return an opening <tr> tag
+	 *
+	 * @since  1.0
+	 *
+	 * @return string  opening <tr> tag with attributes
+	 */
 	public function get_tr_start() {
 		return '<tr valign="top">';
 	}
 
+	/**
+	 * Return a closing </tr> tag
+	 *
+	 * @since  1.0
+	 *
+	 * @return string  closing </tr> tag
+	 */
 	public function get_tr_end() {
 		return '</tr>';
 	}
 
+	/**
+	 * Return an opening <th> tag
+	 *
+	 * @since  1.0
+	 *
+	 * @return string  opening <th> tag with attributes
+	 */
 	public function get_th_start() {
 		return '<th scope="row">';
 	}
 
+	/**
+	 * Return a closing </th> tag
+	 *
+	 * @since  1.0
+	 *
+	 * @return string  closing </th> tag
+	 */
 	public function get_th_end() {
 		return '</th>';
 	}
 
+	/**
+	 * Return an opening <td> tag
+	 *
+	 * @since  1.0
+	 *
+	 * @return string  opening <td> tag
+	 */
 	public function get_td_start() {
 		return '<td>';
 	}
 
+	/**
+	 * Return a closing </td> tag
+	 *
+	 * @since  1.0
+	 *
+	 * @return string  closing </td> tag
+	 */
 	public function get_td_end() {
 		return '</td>';
 	}
 
-	public function get_p( $text ) {
+	/**
+	 * Return string wrapped in a <p> tag
+	 *
+	 * @since  1.0
+	 *
+	 * @param  string  $text Content to wrap in a <p> tag
+	 *
+	 * @return string        Content wrapped in a <p> tag
+	 */
+	public function get_p( $text = '' ) {
 		return '<p>' . $text . '</p>';
 	}
 
+	/**
+	 * Return a form <label> with for attribute
+	 *
+	 * @since  1.0
+	 *
+	 * @param  string  $label_for  form input to associate <label> with
+	 * @param  string  $label_text text to display in the <label> tag
+	 *
+	 * @return string              <label> tag with filled out parts
+	 */
 	public function get_label( $label_for, $label_text ) {
 		$label = '<label for="' . $label_for . '"> ' . $label_text . '</label>';
 
 		return $label;
 	}
 
+	/**
+	 * Return a <span> to indicate required status, with class attribute
+	 *
+	 * @since  1.0
+	 *
+	 * @return string  span tag
+	 */
 	public function get_required() {
 		return '<span class="required">*</span>';
 	}
 
-	public function get_help( $help_text ) {
+	/**
+	 * Return an <a> tag with title attribute holding help text
+	 *
+	 * @since  1.0
+	 *
+	 * @param  string  $help_text Text to use in the title attribute
+	 *
+	 * @return string             <a> tag with filled out parts
+	 */
+	public function get_help( $help_text = '' ) {
 		return '<a href="#" title="' . $help_text . '" class="help">?</a>';
 	}
 
 	/**
-	 * Display a select input with true/false values.
-	 * @param  array  $args values to use in the input
-	 * @return string       constructed input for the form.
+	 * Return a populated <select> input
+	 *
+	 * @since  1.0
+	 *
+	 * @param  array  $args Arguments to use with the <select> input
+	 *
+	 * @return string       Complete <select> input with options and selected attribute.
 	 */
 	public function get_select_bool_input( $args = '' ) {
 		$defaults = $this->get_default_input_parameters(
@@ -105,9 +186,13 @@ class cptui_admin_ui {
 	}
 
 	/**
-	 * Display a text input for the user. Will populate if data is available from options
-	 * @param  array  $args values to use in the input
-	 * @return string       constructed input for the form.
+	 * Return a text input
+	 *
+	 * @since  1.0
+	 *
+	 * @param  array   $args Arguments to use with the text input
+	 *
+	 * @return string        Complete text <input> with proper attributes
 	 */
 	public function get_text_input( $args = '' ) { //TODO: Finish output of other attributes
 		$defaults = $this->get_default_input_parameters(
@@ -143,9 +228,13 @@ class cptui_admin_ui {
 	}
 
 	/**
-	 * Display a textarea input for the user. Will populate if data is available from options
-	 * @param  array  $args values to use in the input
-	 * @return string       constructed input for the form.
+	 * Return a <textarea> input
+	 *
+	 * @since  1.0
+	 *
+	 * @param  array   $args Arguments to use with the textarea input
+	 *
+	 * @return string        Complete <textarea> input with proper attributes
 	 */
 	public function get_textarea_input( $args = '' ) {
 		$defaults = $this->get_default_input_parameters(
@@ -182,9 +271,13 @@ class cptui_admin_ui {
 	}
 
 	/**
-	 * Display a checkbox input for the user. Will check the already selected options if data is available from options.
-	 * @param  array  $args values to use in the input
-	 * @return string       constructed input for the form
+	 * Return a checkbox <input>
+	 *
+	 * @since  1.0
+	 *
+	 * @param  array   $args Arguments to use with the checkbox input
+	 *
+	 * @return string        Complete checkbox <input> with proper attributes
 	 */
 	public function get_check_input( $args = '' ) {
 		$defaults = $this->get_default_input_parameters(
@@ -225,9 +318,13 @@ class cptui_admin_ui {
 	}
 
 	/**
-	 * Return an array of default input values that all input types have.
+	 * Return some array_merged default arguments for all input types
 	 *
-	 * @return array  array of defaults.
+	 * @since  1.0
+	 *
+	 * @param  array   $additions Arguments array to merge with our defaults
+	 *
+	 * @return array             Merged arrays for our default parameters.
 	 */
 	public function get_default_input_parameters( $additions = array() ) {
 		return array_merge(
