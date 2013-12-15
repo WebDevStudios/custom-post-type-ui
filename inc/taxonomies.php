5597e0b7aa3ef91148a0ce867481560bb5cc0d46
Michael Beckwith <tw2113@gmail.com>
2013-12-14 19:10:24 -0600

    create new admin page for editing taxonomies. will handle both adding and editing in the future

--- inc/taxonomies.php
+++ inc/taxonomies.php
@@ -1,3 +1,6 @@
 <?php
-add_submenu_page( 'cpt_main_menu', __( 'Add/Edit Taxonomies', 'cpt-plugin' ), __( 'Add/Edit Taxonomies', 'cpt-plugin' ), 'manage_options', 'cpt_sub_manage_taxonomies', 'cpt_manage_taxonomies' );
-//cpt_settings_tab_menu( $page = 'taxonomies' )
+add_submenu_page( 'cpt_main_menu', __( 'Add/Edit Taxonomies', 'cpt-plugin' ), __( 'Add/Edit Taxonomies', 'cpt-plugin' ), 'manage_options', 'cptui_manage_taxonomies', 'cptui_manage_taxonomies' );
+
+function cptui_manage_taxonomies() {
+	cpt_settings_tab_menu( $page = 'taxonomies' );
+}
