3f680904b407a382f1a94b348ca2e5cbbd6d3f83
Michael Beckwith <tw2113@gmail.com>
2013-12-23 02:22:35 -0600

    two different hooks, to get us rolling

--- inc/post-types.php
+++ inc/post-types.php
@@ -1,5 +1,27 @@
 <?php
-add_submenu_page( 'cpt_main_menu', __( 'Add/Edit Post Types', 'cpt-plugin' ), __( 'Add/Edit Post Types', 'cpt-plugin' ), 'manage_options', 'cptui_manage_post_types', 'cptui_manage_post_types' );
+/**
+ * Add our cptui.js file, with dependencies on jQuery and jQuery UI
+ *
+ * @since  1.9
+ *
+ * @return mixed  js scripts
+ */
+function cpt_post_type_enqueue_scripts() {
+	wp_enqueue_script( 'cptui', plugins_url( 'js/cptui.js' , dirname(__FILE__) ) . '', array( 'jquery', 'jquery-ui-core', 'jquery-ui-accordion' ), '1.9', true );
+}
+add_action( 'admin_enqueue_scripts', 'cpt_post_type_enqueue_scripts' );
+
+/**
+ * Add our settings page to the menu.
+ *
+ * @since  1.9
+ *
+ * @return mixed  new menu
+ */
+function cpt_post_types_admin_menu() {
+	add_submenu_page( 'cpt_main_menu', __( 'Add/Edit Post Types', 'cpt-plugin' ), __( 'Add/Edit Post Types', 'cpt-plugin' ), 'manage_options', 'cptui_manage_post_types', 'cptui_manage_post_types' );
+}
+add_action( 'admin_menu', 'cpt_post_types_admin_menu' );
 
 function cptui_manage_post_types() {
 	cpt_settings_tab_menu();
