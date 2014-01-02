c3811d908f2be33b03fa10b7d91025baba47fa51
Michael Beckwith <tw2113@gmail.com>
2014-01-01 21:04:14 -0600

    fix parent menu slug

--- inc/post-types.php
+++ inc/post-types.php
@@ -22,7 +22,7 @@ add_action( 'admin_enqueue_scripts', 'cpt_post_type_enqueue_scripts' );
  * @return mixed  new menu
  */
 function cpt_post_types_admin_menu() {
-	add_submenu_page( 'cpt_main_menu', __( 'Add/Edit Post Types', 'cpt-plugin' ), __( 'Add/Edit Post Types', 'cpt-plugin' ), 'manage_options', 'cptui_manage_post_types', 'cptui_manage_post_types' );
+	add_submenu_page( 'cptui_main_menu', __( 'Add/Edit Post Types', 'cpt-plugin' ), __( 'Add/Edit Post Types', 'cpt-plugin' ), 'manage_options', 'cptui_manage_post_types', 'cptui_manage_post_types' );
 }
 add_action( 'admin_menu', 'cpt_post_types_admin_menu' );
 
