33aac08c95d7fa2a62f6ff11ac5a580230b79085
Michael Beckwith <tw2113@gmail.com>
2014-01-01 22:23:41 -0600

    more support stuff

--- inc/support.php
+++ inc/support.php
@@ -2,42 +2,50 @@
 
 add_action( 'admin_menu', 'support_admin_menu' );
 function support_admin_menu() {
-	add_submenu_page( 'cpt_main_menu', __( 'Help/Support', 'cpt-plugin' ), __( 'Help/Support', 'cpt-plugin' ), 'manage_options', 'cptui_support', 'cptui_support' );
+	add_submenu_page( 'cptui_main_menu', __( 'Help/Support', 'cpt-plugin' ), __( 'Help/Support', 'cpt-plugin' ), 'manage_options', 'cptui_support', 'cptui_support' );
 }
 
 function cptui_support() {
 	echo '<div class="wrap">';
 	?>
+		<?php
+		/* Ideas:
+			Searching
+			admin sorting
+			admin filtering.
+		 */
+		?>
 		<?php do_action( 'cptui_main_page_before_faq' ); ?>
-		<h1>Frequently Asked Questions</h1>
-			Please note that this plugin will NOT handle display of registered post types or taxonomies in your current theme. It will simply register them for you.
+		<h1>Custom Post Type UI Support</h1>
+				Please note that this plugin will NOT handle display of registered post types or taxonomies in your current theme. It will simply register them for you.
 
-			How can I display content from a custom post type on my website?
-			Justin Tadlock has written some great posts on the topic
-				<a href="http://justintadlock.com/archives/2010/02/02/showing-custom-post-types-on-your-home-blog-page" target="_blank">Showing Custom Post Types on your Home Page</a>
-				<a href="http://justintadlock.com/archives/2010/04/29/custom-post-types-in-wordpress" target="_blank">Custom Post Types in WordPress</a>
+			<div class="questions">
+				<h2>General</h2>
+				<ol>
+					<li>I changed my custom post type name and now I can't get to my posts. How do I get them back?
+					<li>I have added post thumbnail and/or post format support to my post type, but those don't appear when adding a post type post. http://codex.wordpress.org/Function_Reference/add_theme_support
+				</ol>
+				<h2>Frontend Display</h2>
+				<ol>
+					<li>How do I add my custom post type to my frontpage?
+					<li>What template files should I edit to alter my post type display? http://codex.wordpress.org/Template_Hierarchy
+					<li>How do I display my custom post type on my site? http://codex.wordpress.org/Class_Reference/WP_Query
+					<li>I have added categories and tags to my custom post type, but they don't appear in the archives. http://wpmu.org/add-custom-post-types-to-tags-and-categories-in-wordpress/
+				</ol>
+				<h2>Advanced</h2>
+				<ol>
+					<li>How do I add custom metaboxes to my post type? Custom Metaboxes and Fields for WordPress class is a great alternative to a plugin for more advanced users. https://github.com/WebDevStudios/Custom-Metaboxes-and-Fields-for-WordPress
+					<li>How do I add a newly registered taxonomy to a post type that already exists? http://codex.wordpress.org/Function_Reference/register_taxonomy_for_object_type
+					<li>Post relationships http://pippinsplugins.com/introduction-posts-2-posts-plugin/
+					<li>Custom rewrite rules. What they all mean and what you can/can't do
+				</ol>
+			</div>
+			<div class="answers">
+				<ol>
+					<li>You can either change the custom post type name back to the original name or try the Post Type Switcher plugin <a href="http://wordpress.org/extend/plugins/post-type-switcher/" target="_blank">http://wordpress.org/extend/plugins/post-type-switcher/</a>
 
-			How can I add custom meta boxes to my custom post types?
-			The More Fields plugin does a great job at creating custom meta boxes and fully supports custom post types
-				<a href="http://wordpress.org/extend/plugins/more-fields/" target="_blank">http://wordpress.org/extend/plugins/more-fields/</a>.  The Custom Metaboxes and Fields for WordPress class is a great alternative to a plugin for more advanced users.
-
-			I changed my custom post type name and now I can't get to my posts
-			You can either change the custom post type name back to the original name or try the Post Type Switcher plugin
-				<a href="http://wordpress.org/extend/plugins/post-type-switcher/" target="_blank">http://wordpress.org/extend/plugins/post-type-switcher/</a>
-
-
-			Template Hierarchy
-				http://codex.wordpress.org/Template_Hierarchy
-			WP_Query
-				http://codex.wordpress.org/Class_Reference/WP_Query
-			CPTs in Cat/Tag archives
-				http://wpmu.org/add-custom-post-types-to-tags-and-categories-in-wordpress/
-			add_theme_support()
-				post-formats
-				post-thumbnails
-				http://codex.wordpress.org/Function_Reference/add_theme_support
-
-
+				</ol>
+			</div>
 		<?php do_action( 'cptui_main_page_after_faq' ); ?>
 	<?php
 	echo '</div>';
