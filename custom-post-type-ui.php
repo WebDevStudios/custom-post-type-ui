3f8a453b1c6438329e48aa8a7c0db7c9cb2af12f
Michael Beckwith <tw2113@gmail.com>
2014-01-04 19:57:56 -0600

    make paypal translateable. Would they all know what to do with this if we did not?!?!?

--- custom-post-type-ui.php
+++ custom-post-type-ui.php
@@ -287,7 +287,7 @@ function cptui_settings() { ?>
 					<form action="https://www.paypal.com/cgi-bin/webscr" method="post">
 					<input type="hidden" name="cmd" value="_s-xclick">
 					<input type="hidden" name="hosted_button_id" value="YJEDXPHE49Q3U">
-					<input type="image" src="https://www.paypal.com/en_US/i/btn/btn_donateCC_LG.gif" border="0" name="submit" alt="PayPal - The safer, easier way to pay online!">
+					<input type="image" src="https://www.paypal.com/en_US/i/btn/btn_donateCC_LG.gif" border="0" name="submit" alt="<?php esc_attr_e( 'PayPal - The safer, easier way to pay online!', 'cpt-plugin' ); ?>">
 					<img alt="" border="0" src="https://www.paypal.com/en_US/i/scr/pixel.gif" width="1" height="1">
 					</form>
 					</p>
