1c5eba6e51d69d9d6842942ed47fffd38a7d6603
Michael Beckwith <tw2113@gmail.com>
2014-01-08 21:51:17 -0600

    create our delete confirmation prompt

--- js/cptui.js
+++ js/cptui.js
@@ -1,3 +1,12 @@
 (function($) {
+	//Create our accordions
 	$( "#cptui_accordion" ).accordion({ collapsible: true, heightStyle: 'fill', active: 2 });
+
+	//confirm our deletions
+	$( '#cpt_submit_delete' ).on( 'click', function() {
+		if( confirm( 'Are you sure you want to delete this?' ) ) {
+			return true;
+		}
+		return false;
+	});
 })(jQuery);
