c424b99fbae77a3aca822d80cf773969f8d16afc
Michael Beckwith <tw2113@gmail.com>
2014-12-31 15:19:04 -0600

    filling in answers for provided FAQs

--- inc/support.php
+++ inc/support.php
@@ -50,28 +50,33 @@ function cptui_support() {
 						</li>
 						<li>
 							<span class="question"><?php _e( 'I have added post thumbnail and/or post format support to my post type, but those do not appear when adding a post type post.', 'cpt-plugin' ); ?></span>
-							<div class="answer">http://codex.wordpress.org/Function_Reference/add_theme_support</div>
+							<div class="answer"><?php _e( 'Make sure your theme has post "post-thumbnails" theme support enabled.', 'cpt-plugin' ); ?> <a href="http://codex.wordpress.org/Function_Reference/add_theme_support" target="_blank">http://codex.wordpress.org/Function_Reference/add_theme_support</a></div>
 						</li>
 					</ol>
 				</td>
 				<td>
-					<h2><?php _e( 'Frontend Display', 'cpt-plugin' ); ?></h2>
+					<h2><?php _e( 'Front-end Display', 'cpt-plugin' ); ?></h2>
 					<ol id="questions_front">
 						<li>
-							<span class="question"><?php _e( 'How do I add my custom post type to my frontpage?', 'cpt-plugin' ); ?></span>
-							<div class="answer"></div>
-						</li>
-						<li>
 							<span class="question"><?php _e( 'What template files should I edit to alter my post type display?', 'cpt-plugin' ); ?></span>
-							<div class="answer">http://codex.wordpress.org/Template_Hierarchy</div>
+							<div class="answer"><?php printf( __( 'Please visit the %sTemplate Hierarchy%s page on the WordPress codex for details about available templates.', 'cpt-plugin' ),
+							'<a href="http://codex.wordpress.org/Template_Hierarchy" target="_blank">',
+							'</a>'
+							); ?>
+							</div>
 						</li>
 						<li>
 							<span class="question"><?php _e( 'How do I display my custom post type on my site?', 'cpt-plugin' ); ?></span>
-							<div class="answer">http://codex.wordpress.org/Class_Reference/WP_Query</div>
+							<div class="answer"><?php printf( __( 'You will need to utilize the %sWP_Query%s class to handle display in custom locations. If you have set the post type to have archives, the archive url should be something like "http://www.mysite.com/post-type-slug"', 'cpt-plugin' ),
+							'<a href="http://codex.wordpress.org/Class_Reference/WP_Query" target="_blank">',
+							'</a>'
+							); ?></div>
 						</li>
 						<li>
 							<span class="question"><?php _e( 'I have added categories and tags to my custom post type, but they do not appear in the archives.', 'cpt-plugin' ); ?></span>
-							<div class="answer">http://wpmu.org/add-custom-post-types-to-tags-and-categories-in-wordpress/</div>
+							<div class="answer"><?php printf( __( 'You will need to add your newly created post type to the types that the category and tag archives query for. You can see a tutorial on how to do that at %s', 'cpt-plugin' ),
+								'<a href="http://wpmu.org/add-custom-post-types-to-tags-and-categories-in-wordpress/" target="_blank">http://wpmu.org/add-custom-post-types-to-tags-and-categories-in-wordpress/</a>'
+							); ?> </div>
 						</li>
 					</ol>
 				</td>
@@ -82,19 +87,29 @@ function cptui_support() {
 					<ol id="questions_advanced">
 						<li>
 							<span class="question"><?php _e( 'How do I add custom metaboxes to my post type?', 'cpt-plugin' ); ?></span>
-							<div class="answer"><?php _e( 'Custom Metaboxes and Fields for WordPress class is a great alternative to a plugin for more advanced users.', 'cpt-plugin' ); ?> https://github.com/WebDevStudios/Custom-Metaboxes-and-Fields-for-WordPress</div>
+							<div class="answer"><?php printf(
+									__( 'We recommend checking out %s, the latest iteration of "Custom Metaboxes and Fields for WordPress". Both are maintained by WebDevStudios.', 'cpt-plugin' ),
+								'<a href="https://wordpress.org/plugins/cmb2/" target="_blank">CMB2</a>'
+							); ?>
+							</div>
 						</li>
 						<li>
 							<span class="question"><?php _e( 'How do I add a newly registered taxonomy to a post type that already exists?', 'cpt-plugin' ); ?></span>
-							<div class="answer">http://codex.wordpress.org/Function_Reference/register_taxonomy_for_object_type</div>
+							<div class="answer"><?php printf(
+								__( 'Check out the %s function for documentation and usage examples.'),
+								'<a href="http://codex.wordpress.org/Function_Reference/register_taxonomy_for_object_type" target="_blank">register_taxonomy_for_object_type()</a>'
+								); ?>
+							</div>
 						</li>
 						<li>
 							<span class="question"><?php _e( 'Post relationships?', 'cpt-plugin' ); ?></span>
-							<div class="answer">http://pippinsplugins.com/introduction-posts-2-posts-plugin/</div>
-						</li>
-						<li>
-							<span class="question"><?php _e( 'Custom rewrite rules. What they all mean and what you can/cannot do', 'cpt-plugin' ); ?></span>
-							<div class="answer"></div>
+							<div class="answer"><?php printf( __( '%s has an excellent %spost%s introducing users to the %sPosts 2 Posts%s plugin that should be a good start.', 'cpt-plugin' ),
+							'Pippin Williamson',
+							'<a href="http://pippinsplugins.com/introduction-posts-2-posts-plugin/" target="_blank">',
+							'</a>',
+							'<a href="https://wordpress.org/plugins/posts-to-posts/" target="_blank">',
+							'</a>'
+							); ?></div>
 						</li>
 						<li>
 							<span class="question"><?php _e( 'How do I filter the "enter title here" text in the post editor screen.', 'cpt-plugin' ); ?></span>
