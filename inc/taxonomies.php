4329b7e14dd5ebc1949d85e4b8213c8047a26873
Michael Beckwith <tw2113@gmail.com>
2015-01-03 14:47:54 -0600

    alignment and query_var_slug param

--- inc/taxonomies.php
+++ inc/taxonomies.php
@@ -570,16 +570,17 @@ function cptui_update_taxonomy( $data = array() ) {
 	}
 
 	$taxonomies[ $data['cpt_custom_tax']['name'] ] = array(
-		'name' => $data['cpt_custom_tax']['name'],
-		'label' => $data['cpt_custom_tax']['label'],
-		'singular_label' => $data['cpt_custom_tax']['singular_label'],
-		'hierarchical' => disp_boolean( $data['cpt_custom_tax']['hierarchical'] ),
-		'show_ui' => disp_boolean( $data['cpt_custom_tax']['show_ui'] ),
-		'query_var' => disp_boolean( $data['cpt_custom_tax']['query_var'] ),
-		'rewrite' => disp_boolean( $data['cpt_custom_tax']['rewrite'] ),
-		'rewrite_slug' => $data['cpt_custom_tax']['rewrite_slug'],
+		'name'              => $data['cpt_custom_tax']['name'],
+		'label'             => $data['cpt_custom_tax']['label'],
+		'singular_label'    => $data['cpt_custom_tax']['singular_label'],
+		'hierarchical'      => disp_boolean( $data['cpt_custom_tax']['hierarchical'] ),
+		'show_ui'           => disp_boolean( $data['cpt_custom_tax']['show_ui'] ),
+		'query_var'         => disp_boolean( $data['cpt_custom_tax']['query_var'] ),
+		'query_var_slug'    => $data['cpt_custom_tax']['query_var_slug'],
+		'rewrite'           => disp_boolean( $data['cpt_custom_tax']['rewrite'] ),
+		'rewrite_slug'      => $data['cpt_custom_tax']['rewrite_slug'],
 		'show_admin_column' => disp_boolean( $data['cpt_custom_tax']['show_admin_column'] ),
-		'labels' => $data['cpt_tax_labels']
+		'labels'            => $data['cpt_tax_labels']
 	);
 
 	$taxonomies[ $data['cpt_custom_tax']['name'] ]['object_type'] = $data['cpt_post_types'];
