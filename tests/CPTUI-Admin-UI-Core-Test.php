fae73d18a07de8286b5b226557a6fe1c74e883e7
Michael Beckwith <tw2113@gmail.com>
2015-01-08 23:18:55 -0600

    re-add test_ prefix for tests to differentiate from helper methods

--- tests/CPTUI-Admin-UI-Core-Test.php
+++ tests/CPTUI-Admin-UI-Core-Test.php
@@ -15,18 +15,15 @@ class CPTUI_Admin_UI_Core extends WP_UnitTestCase {
 
 	/**
 	 * Tests for our file being present and available.
-	 * @test
 	 */
-	public function CPTUI_Admin_UI_Exists() {
+	public function test_CPTUI_Admin_UI_Exists() {
 		$this->assertFileExists( CPTUI_DIRECTORY_PATH . '/classes/class.cptui_admin_ui.php' );
 	}
 
-	/*
+	/**
 	 * Test that we are able to instantiate our class and get an object back.
-	 *
-	 * @test
 	 */
-	public function CPTUI_Admin_UI_Objects() {
+	public function test_CPTUI_Admin_UI_Objects() {
 		$ui = new cptui_admin_ui();
 		$this->assertInstanceOf( 'cptui_admin_ui', $ui );
 	}
