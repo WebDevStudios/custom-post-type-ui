5b462fb1321a274b0a514e6d83a86e10af8c78fb
Michael Beckwith <tw2113@gmail.com>
2015-01-08 23:45:53 -0600

    moving and grouping our taxonomy based test methods

--- tests/CPTUI-Utilities-Test.php
+++ tests/CPTUI-Utilities-Test.php
@@ -45,13 +45,6 @@ class CPTUI_Utility extends WP_UnitTestCase {
 	}
 
 	/**
-	 * Tests that we have our needed taxonomy functions
-	 */
-	public function test_CPTUI_taxonomy_registration_function_exists() {
-		$this->assertTrue( function_exists( 'cptui_register_single_taxonomy' ) );
-	}
-
-	/**
 	 * Tests for successful registering of post types with our own custom functions.
 	 */
 	public function test_CPTUI_registered_post_type() {
@@ -88,6 +81,50 @@ class CPTUI_Utility extends WP_UnitTestCase {
 		$this->assertEquals( 'Parent Movie', $movie->labels->parent );
 	}
 
+	/**
+	 * Tests our setting of our taxonomy option and retrieval afterwards.
+	 */
+	public function test_CPTUI_set_taxonomy_option() {
+		$this->assertTrue( is_array( $this->taxonomy_array ), 'Option returned false' );
+		$this->assertArrayHasKey( 'actors', $this->taxonomy_array, 'Actors was not found in returned option value' );
+	}
+
+	/**
+	 * Tests that we have our needed taxonomy functions
+	 */
+	public function test_CPTUI_taxonomy_registration_function_exists() {
+		$this->assertTrue( function_exists( 'cptui_register_single_taxonomy' ) );
+	}
+
+	/**
+	 * Tests for successful registering of taxonomies with our own custom functions.
+	 */
+	public function test_CPTUI_registered_taxonomy() {
+		$this->register_post_type();
+		$this->register_taxonomy();
+
+		$registered = get_taxonomies();
+		$this->assertArrayHasKey( 'actors', $registered );
+	}
+
+	/**
+	 * Tests for our registered labels for the taxonomy.
+	 */
+	public function test_CPTUI_registered_taxonomy_labels_names() {
+		$this->register_post_type();
+		$this->register_taxonomy();
+
+		$actor = get_taxonomy( 'actors' );
+		$this->assertTrue( is_object( $actor ) );
+		$this->assertTrue( is_object( $actor->labels ) );
+		$this->assertNotEmpty( $actor->labels, 'No labels available' );
+
+	}
+
+	/**
+	 * Create our base post type to test.
+	 * @return mixed|void
+	 */
 	public function setup_cpt_option() {
 
 		$old = 'a:1:{s:5:"movie";a:21:{s:4:"name";s:5:"movie";s:5:"label";s:6:"Movies";s:14:"singular_label";s:5:"Movie";s:11:"description";s:0:"";s:6:"public";s:4:"true";s:7:"show_ui";s:4:"true";s:11:"has_archive";s:5:"false";s:19:"exclude_from_search";s:5:"false";s:15:"capability_type";s:4:"post";s:12:"hierarchical";s:5:"false";s:7:"rewrite";s:4:"true";s:12:"rewrite_slug";s:0:"";s:17:"rewrite_withfront";s:4:"true";s:9:"query_var";s:4:"true";s:13:"menu_position";s:0:"";s:12:"show_in_menu";s:4:"true";s:19:"show_in_menu_string";s:0:"";s:9:"menu_icon";N;s:8:"supports";a:0:{}s:10:"taxonomies";a:0:{}s:6:"labels";a:13:{s:9:"menu_name";s:9:"My Movies";s:9:"all_items";s:10:"All Movies";s:7:"add_new";s:7:"Add New";s:12:"add_new_item";s:13:"Add New Movie";s:4:"edit";s:4:"Edit";s:9:"edit_item";s:10:"Edit Movie";s:8:"new_item";s:9:"New Movie";s:4:"view";s:4:"View";s:9:"view_item";s:10:"View Movie";s:12:"search_items";s:13:"Search Movies";s:9:"not_found";s:15:"No Movies Found";s:18:"not_found_in_trash";s:24:"No Movies found in trash";s:6:"parent";s:12:"Parent Movie";}}}';
