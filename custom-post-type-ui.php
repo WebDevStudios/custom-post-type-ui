7cb80a168c15e5c7d949bf948d279de2f6e90f24
Michael Beckwith <tw2113@gmail.com>
2015-02-04 10:00:21 -0600

    update query_var checking to make sure we are not explicity passing in a bool EVERY time, like when a string was purposely provided

--- custom-post-type-ui.php
+++ custom-post-type-ui.php
@@ -163,6 +163,10 @@ function cptui_register_single_post_type( $post_type = array() ) {
 		}
 	}
 
+	if ( in_array( $post_type['query_var'], array( 'true', 'false', '0', '1' ) ) ) {
+		$post_type['query_var'] = get_disp_boolean( $post_type['query_var'] );
+	}
+
 	$args = array(
 		'labels'              => $labels,
 		'description'         => $post_type['description'],
@@ -177,7 +181,7 @@ function cptui_register_single_post_type( $post_type = array() ) {
 		'rewrite'             => $rewrite,
 		'menu_position'       => $post_type['menu_position'],
 		'menu_icon'           => $post_type['menu_icon'],
-		'query_var'           => get_disp_boolean( $post_type['query_var'] ),
+		'query_var'           => $post_type['query_var'],
 		'supports'            => $post_type['supports'],
 		'taxonomies'          => $post_type['taxonomies']
 	);
