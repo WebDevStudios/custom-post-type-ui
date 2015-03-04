65dae167992c792f9a63d04d457ed4483681d387
Michael Beckwith <tw2113@gmail.com>
2015-03-04 16:52:03 -0600

    fix the boolean order to properly match false default for hierarchical rewrites on taxonomies

--- custom-post-type-ui.php
+++ custom-post-type-ui.php
@@ -261,7 +261,7 @@ function cptui_register_single_taxonomy( $taxonomy = array() ) {
 
 		$rewrite['with_front'] = ( ! empty( $taxonomy['rewrite_withfront'] ) && 'false' === disp_boolean( $taxonomy['rewrite_withfront'] ) ) ? false : true;
 
-		$rewrite['hierarchical'] = ( ! empty( $taxonomy['rewrite_hierarchical'] ) && 'false' === disp_boolean( $taxonomy['rewrite_hierarchical'] ) ) ? false : true;
+		$rewrite['hierarchical'] = ( ! empty( $taxonomy['rewrite_hierarchical'] ) && 'true' === disp_boolean( $taxonomy['rewrite_hierarchical'] ) ) ? true : false;
 	}
 
 	if ( in_array( $taxonomy['query_var'], array( 'true', 'false', '0', '1' ) ) ) {
