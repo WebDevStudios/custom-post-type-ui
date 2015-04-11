fd01275256191e9ea2a85d2d5f04082bd9ac8c94
Michael Beckwith <tw2113@gmail.com>
2015-04-10 19:19:10 -0500

    remove accordian js usage

--- js/cptui.js
+++ js/cptui.js
@@ -1,7 +1,4 @@
 (function($) {
-	//Create our accordions
-	$( "#cptui_accordion" ).accordion({ collapsible: true, heightStyle: 'fill', active: 2 });
-
 	//confirm our deletions
 	$( '#cpt_submit_delete' ).on( 'click', function() {
 		if( confirm( confirmdata.confirm ) ) {
