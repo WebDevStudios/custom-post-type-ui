46ee8702c75000bc9e78ed8893ce2f3b0a826345
Michael Beckwith <tw2113@gmail.com>
2015-04-16 00:03:52 -0500

    remove redundent click headings note

--- inc/taxonomies.php
+++ inc/taxonomies.php
@@ -186,8 +186,6 @@ function cptui_manage_taxonomies() {
 				</p>
 			</td>
 			<td>
-				<p><?php _e( 'Click headings to reveal available options.', 'cpt-plugin' ); ?></p>
-
 				<div>
 					<h3 title="<?php esc_attr_e( 'Click to expand', 'cpt-plugin' ); ?>"><?php _e( 'Labels', 'cpt-plugin' ); ?></h3>
 						<div>
