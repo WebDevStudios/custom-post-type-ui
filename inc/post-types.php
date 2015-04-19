99267b265cad3ebe5fb0a17693f123bf2e989b9b
Michael Beckwith <tw2113@gmail.com>
2015-04-18 20:08:01 -0500

    adding some bits even though we are leaving for a future release

--- inc/post-types.php
+++ inc/post-types.php
@@ -19,8 +19,19 @@ function cptui_post_type_enqueue_scripts() {
 		return;
 	}
 
+	/*$post_types = get_option( 'cptui_post_types' );
+	foreach( $post_types as $type ) {
+		$types[] = $type['name'];
+	}*/
+
 	wp_enqueue_script( 'cptui', plugins_url( 'js/cptui.js', dirname(__FILE__) ), array( 'jquery', 'jquery-ui-core', 'jquery-ui-accordion' ), CPT_VERSION, true );
-	wp_localize_script(	'cptui', 'confirmdata', array( 'confirm' => __( 'Are you sure you want to delete this?', 'cpt-plugin' ) ) );
+	wp_localize_script(	'cptui', 'cptui_type_data',
+		array(
+			'confirm' => __( 'Are you sure you want to delete this?', 'cpt-plugin' ),
+			# 'post_types' => $types,
+			#'post_change_name' => '<div class="typetax-rename">' . __( 'Changing this will rename the post type.', 'cpt-plugin' ) . '</div>'
+		)
+	);
 }
 add_action( 'admin_enqueue_scripts', 'cptui_post_type_enqueue_scripts' );
 
