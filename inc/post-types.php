e657bc6e17e30fac0c57e6d1433e4f2814fae195
Michael Beckwith <tw2113@gmail.com>
2015-06-20 23:10:36 -0500

    set default parameter value and return first found key if we recently deleted a post type

--- inc/post-types.php
+++ inc/post-types.php
@@ -923,12 +923,19 @@ function cptui_post_types_dropdown( $post_types = array() ) {
  *
  * @since 1.0.0
  *
+ * @param bool $post_type_deleted Whether or not a post type was recently deleted.
+ *
  * @return bool|string $value False on no result, sanitized post type if set.
  */
-function cptui_get_current_post_type() {
+function cptui_get_current_post_type( $post_type_deleted = false ) {
 	if ( ! empty( $_POST ) ) {
 		if ( isset( $_POST['cptui_selected_post_type']['post_type'] ) ) {
 			return sanitize_text_field( $_POST['cptui_selected_post_type']['post_type'] );
+		}
+
+		if ( $post_type_deleted ) {
+			$post_types = get_option( 'cptui_post_types' );
+			return key( $post_types );
 		}
 
 		if ( isset( $_POST['cpt_custom_post_type']['name'] ) ) {
