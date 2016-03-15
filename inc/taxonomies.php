f3e495aba2be491b8b3f183c1c8b975d16be2d59
Michael Beckwith <tw2113@gmail.com>
2016-03-15 00:53:34 -0500

    add aria-selected attribute to array of tab data

--- inc/taxonomies.php
+++ inc/taxonomies.php
@@ -62,14 +62,16 @@ function cptui_taxonomy_tabs( $tabs = array(), $current_page = '' ) {
 		$tabs['tabs']       = array();
 		// Start out with our basic "Add new" tab.
 		$tabs['tabs']['add'] = array(
-			'text'    => __( 'Add New Taxonomy', 'custom-post-type-ui' ),
-			'classes' => $classes,
-			'url'     => admin_url( 'admin.php?page=cptui_manage_' . $current_page )
+			'text'          => __( 'Add New Taxonomy', 'custom-post-type-ui' ),
+			'classes'       => $classes,
+			'url'           => admin_url( 'admin.php?page=cptui_manage_' . $current_page ),
+			'aria-selected' => 'false'
 		);
 
 		$action = cptui_get_current_action();
 		if ( empty( $action ) ) {
 			$tabs['tabs']['add']['classes'][] = 'nav-tab-active';
+			$tabs['tabs']['add']['aria-selected'] = 'true';
 		}
 
 		if ( ! empty( $taxonomies ) ) {
@@ -78,15 +80,17 @@ function cptui_taxonomy_tabs( $tabs = array(), $current_page = '' ) {
 				$classes[] = 'nav-tab-active';
 			}
 			$tabs['tabs']['edit'] = array(
-				'text'    => __( 'Edit Taxonomies', 'custom-post-type-ui' ),
-				'classes' => $classes,
-				'url'     => esc_url( add_query_arg( array( 'action' => 'edit' ), admin_url( 'admin.php?page=cptui_manage_' . $current_page ) ) )
+				'text'          => __( 'Edit Taxonomies', 'custom-post-type-ui' ),
+				'classes'       => $classes,
+				'url'           => esc_url( add_query_arg( array( 'action' => 'edit' ), admin_url( 'admin.php?page=cptui_manage_' . $current_page ) ) ),
+				'aria-selected' => ( ! empty( $action ) ) ? 'true' : 'false'
 			);
 
 			$tabs['tabs']['view'] = array(
-				'text'    => __( 'View Taxonomies', 'custom-post-type-ui' ),
-				'classes' => array( 'nav-tab' ), // Prevent notices.
-				'url'     => esc_url( admin_url( 'admin.php?page=cptui_listings#taxonomies' ) )
+				'text'          => __( 'View Taxonomies', 'custom-post-type-ui' ),
+				'classes'       => array( 'nav-tab' ), // Prevent notices.
+				'url'           => esc_url( admin_url( 'admin.php?page=cptui_listings#taxonomies' ) ),
+				'aria-selected' => 'false'
 			);
 		}
 	}
