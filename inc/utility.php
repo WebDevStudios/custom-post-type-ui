7cc17b0037f55e9c22f715abd2077d459c6a85dc
Michael Beckwith <tw2113@gmail.com>
2016-03-18 14:59:42 -0500

    remove functions we decided not to utilize, docs for the ones we are

--- inc/utility.php
+++ inc/utility.php
@@ -323,76 +323,24 @@ function cptui_post_form_action( $ui ) {
 	echo cptui_get_post_form_action( $ui );
 }
 
+/**
+ * Fetch our CPTUI post types option.
+ *
+ * @since 1.3.0
+ *
+ * @return mixed|void
+ */
 function cptui_get_post_type_data() {
 	return apply_filters( 'cptui_get_post_type_data', get_option( 'cptui_post_types', array() ), get_current_blog_id() );
 }
 
+/**
+ * Fetch our CPTUI taxonomies option.
+ *
+ * @since 1.3.0
+ *
+ * @return mixed|void
+ */
 function cptui_get_taxonomy_data() {
 	return apply_filters( 'cptui_get_taxonomy_data', get_option( 'cptui_taxonomies', array() ), get_current_blog_id() );
-}
-
-/**
- * Returns the CPTUI post types option.
- *
- * @since 1.3.0
- *
- * @return mixed|void
- */
-function cptui_post_type_dropdown_switch_data() {
-	return apply_filters( 'cptui_post_type_dropdown_switch_data', get_option( 'cptui_post_types', array() ), get_current_blog_id() );
-}
-
-/**
- * Returns the CPTUI taxonomies option.
- *
- * @since 1.3.0
- *
- * @return mixed|void
- */
-function cptui_taxonomy_dropdown_switch_data() {
-	return apply_filters( 'cptui_taxonomy_dropdown_switch_data', get_option( 'cptui_taxonomies', array() ), get_current_blog_id() );
-}
-
-/**
- * Deletes the CPTUI post types option.
- *
- * @since 1.3.0
- *
- * @return mixed|void
- */
-function cptui_delete_post_types_option() {
-	return apply_filters( 'cptui_delete_post_types_option', delete_option( 'cptui_post_types' ), get_current_blog_id() );
-}
-
-/**
- * Deletes the CPTUI taxonomies option.
- *
- * @since 1.3.0
- *
- * @return mixed|void
- */
-function cptui_delete_taxonomies_option() {
-	return apply_filters( 'cptui_delete_taxonomies_option', delete_option( 'cptui_taxonomies' ), get_current_blog_id() );
-}
-
-/**
- * Updates the CPTUI post types option.
- *
- * @param array $post_types Array of post types to save.
- *
- * @return mixed|void
- */
-function cptui_update_post_types_option( $post_types = array() ) {
-	return apply_filters( 'cptui_update_post_types_option', update_option( 'cptui_post_types', $post_types ), get_current_blog_id() );
-}
-
-/**
- * Updates the CPTUI taxonomies option.
- *
- * @param array $taxonomies Array of taxonomies to save.
- *
- * @return mixed|void
- */
-function cptui_update_taxonomies_option( $taxonomies = array() ) {
-	return apply_filters( 'cptui_update_taxonomies_option', update_option( 'cptui_taxonomies', $taxonomies ), get_current_blog_id() );
 }
