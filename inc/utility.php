d424ea1fef51e98ba574ed6a0a44d25be39ce0d2
Michael Beckwith <tw2113@gmail.com>
2016-04-26 01:19:12 -0500

    add new function and output for newsletter signup

--- inc/utility.php
+++ inc/utility.php
@@ -287,6 +287,7 @@ function cptui_get_post_type_exists( $slug = '', $data = array() ) {
  * @internal
  */
 function cptui_products_sidebar() {
+
 	if ( false === ( $ads = get_transient( 'wds_promos' ) ) ) {
 		$ads = wp_remote_get( 'https://webdevstudios.com/assets/wds.json' );
 
@@ -298,6 +299,9 @@ function cptui_products_sidebar() {
 
 	if ( ! empty( $ads ) ) {
 		echo '<div class="wdspromos">';
+
+		cptui_newsletter_form();
+
 		foreach ( $ads as $ad ) {
 			$the_ad = $ad->text;
 			$image = wp_remote_get( $ad->image );
@@ -327,3 +331,41 @@ function cptui_products_sidebar() {
 }
 add_action( 'cptui_below_post_type_tab_menu', 'cptui_products_sidebar' );
 add_action( 'cptui_below_taxonomy_tab_menu', 'cptui_products_sidebar' );
+
+function cptui_newsletter_form() {
+	?>
+<!-- Begin MailChimp Signup Form -->
+<link href="//cdn-images.mailchimp.com/embedcode/classic-10_7.css" rel="stylesheet" type="text/css">
+<div id="mc_embed_signup">
+	<form action="//webdevstudios.us1.list-manage.com/subscribe/post?u=67169b098c99de702c897d63e&amp;id=9cb1c7472e" method="post" id="mc-embedded-subscribe-form" name="mc-embedded-subscribe-form" class="validate" target="_blank" novalidate>
+		<div id="mc_embed_signup_scroll">
+			<h2>Subscribe to our mailing list</h2>
+			<div class="indicates-required"><span class="asterisk">*</span> indicates required</div>
+			<div class="mc-field-group">
+				<label for="mce-EMAIL">Email Address <span class="asterisk">*</span>
+				</label>
+				<input type="email" value="" name="EMAIL" class="required email" id="mce-EMAIL">
+			</div>
+			<div id="mce-responses" class="clear">
+				<div class="response" id="mce-error-response" style="display:none"></div>
+				<div class="response" id="mce-success-response" style="display:none"></div>
+			</div>    <!-- real people should not fill this in and expect good things - do not remove this or risk form bot signups-->
+			<div style="position: absolute; left: -5000px;" aria-hidden="true">
+				<input type="text" name="b_67169b098c99de702c897d63e_9cb1c7472e" tabindex="-1" value=""></div>
+			<div class="clear">
+				<input type="submit" value="Subscribe" name="subscribe" id="mc-embedded-subscribe" class="button">
+			</div>
+		</div>
+	</form>
+</div>
+<script type='text/javascript' src='//s3.amazonaws.com/downloads.mailchimp.com/js/mc-validate.js'></script>
+<script type='text/javascript'>(function ($) {
+		window.fnames = new Array();
+		window.ftypes = new Array();
+		fnames[0] = 'EMAIL';
+		ftypes[0] = 'email';
+	}(jQuery));
+	var $mcj = jQuery.noConflict(true);</script>
+<!--End mc_embed_signup-->
+<?php
+}
