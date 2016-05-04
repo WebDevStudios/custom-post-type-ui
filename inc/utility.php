151efd5bce3b021a0fdfc3b1ec74361db29ddf12
Michael Beckwith <tw2113@gmail.com>
2016-05-04 02:15:52 -0500

    update the remove ad link

--- inc/utility.php
+++ inc/utility.php
@@ -317,7 +317,7 @@ function cptui_products_sidebar() {
 		}
 		printf(
 			'<p><a href="%s">%s</a></p>',
-			'https://pluginize.com/product/custom-post-type-ui-extended/?utm_source=cptui-dashboard&utm_medium=text&utm_campaign=remove_ads',
+			'https://pluginize.com/product/custom-post-type-ui-extended/?utm_source=remove_ads&utm_medium=text&utm_campaign=cptui',
 			esc_html__( 'Remove these ads?', 'custom-post-type-ui' )
 		);
 		echo '</div>';
