6b9d20417c9f93583c14a584bfffca23bb087336
Michael Beckwith <tw2113@gmail.com>
2016-05-14 23:18:18 -0500

    version 3.4 sites should not be able to upgrade to our current version. No need for bac compat

--- classes/class.cptui_debug_info.php
+++ classes/class.cptui_debug_info.php
@@ -42,13 +42,8 @@ class CPTUI_Debug_Info {
 
 		global $wpdb;
 
-		if ( get_bloginfo( 'version' ) < '3.4' ) {
-			$theme_data = get_theme_data( get_stylesheet_directory() . '/style.css' );
-			$theme = $theme_data['Name'] . ' ' . $theme_data['Version'];
-		} else {
-			$theme_data = wp_get_theme();
-			$theme = $theme_data->Name . ' ' . $theme_data->Version;
-		}
+		$theme_data = wp_get_theme();
+		$theme = $theme_data->Name . ' ' . $theme_data->Version;
 
 		ob_start();
 		?>
