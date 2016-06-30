c7605ce4673be095338c12b3fc1826e2bc542b83
Michael Beckwith <tw2113@gmail.com>
2016-06-30 15:23:39 -0500

    merging in issue #498, newsletter on the about page

--- inc/about.php
+++ inc/about.php
@@ -46,6 +46,15 @@ function cptui_settings() {
 		<div class="about-text cptui-about-text">
 			<?php _e( 'Thank you for choosing Custom Post Type UI! We hope that your experience with our plugin makes creating post types and taxonomies and organizing your content quick and easy.', 'custom-post-type-ui' ); ?>
 		</div>
+
+		<?php
+		/**
+		 * Fires before the About Page changelog.
+		 *
+		 * @since 1.4.0
+		 */
+		do_action( 'cptui_main_page_before_changelog' ); ?>
+
 		<h2><?php printf( __( 'What\'s new in version %s', 'custom-post-type-ui' ), CPTUI_VERSION ); ?></h2>
 		<div class="changelog about-integrations">
 			<div class="cptui-feature feature-section col three-col">
@@ -120,3 +129,57 @@ function cptui_pluginize_content() {
 	echo '</div>';
 }
 add_action( 'cptui_main_page_extra_notes', 'cptui_pluginize_content', 9 );
+
+function cptui_about_page_newsletter() {
+	?>
+	<h3><?php esc_html_e( 'Stay informed', 'custom-post-type-ui' ); ?></h3>
+	<?php
+	cptui_about_page_newsletter_form();
+}
+add_action( 'cptui_main_page_before_changelog', 'cptui_about_page_newsletter' );
+
+/**
+ * Outputs our newsletter signup form.
+ *
+ * @since 1.4.0
+ *
+ * @internal
+ */
+function cptui_about_page_newsletter_form() {
+	?>
+	<!-- Begin MailChimp Signup Form -->
+	<link href="//cdn-images.mailchimp.com/embedcode/classic-10_7.css" rel="stylesheet" type="text/css">
+	<div id="mc_embed_signup">
+		<form action="//webdevstudios.us1.list-manage.com/subscribe/post?u=67169b098c99de702c897d63e&amp;id=9cb1c7472e" method="post" id="mc-embedded-subscribe-form" name="mc-embedded-subscribe-form" class="validate" target="_blank" novalidate>
+			<div id="mc_embed_signup_scroll">
+
+				<p>
+					<strong><?php esc_html_e( 'Wanna make the most of WordPress? Sign up for the Pluginize newsletter and get access to discounts, plugin announcements, and more!', 'custom-post-type-ui' ); ?></strong>
+				</p>
+				<div class="mc-field-group">
+					<label for="mce-EMAIL"><?php esc_html_e( 'Email Address', 'custom-post-type-ui' ); ?></label>
+					<input tabindex="-1" type="email" value="" name="EMAIL" class="required email" id="mce-EMAIL">
+				</div>
+				<div id="mce-responses" class="clear">
+					<div class="response" id="mce-error-response" style="display:none"></div>
+					<div class="response" id="mce-success-response" style="display:none"></div>
+				</div>    <!-- real people should not fill this in and expect good things - do not remove this or risk form bot signups-->
+				<div style="position: absolute; left: -5000px;" aria-hidden="true">
+					<input type="text" name="b_67169b098c99de702c897d63e_9cb1c7472e" tabindex="-1" value=""></div>
+				<div class="clear">
+					<input type="submit" value="<?php esc_attr_e( 'Subscribe', 'custom-post-type-ui' ); ?>" name="subscribe" id="mc-embedded-subscribe" class="button" tabindex="-1">
+				</div>
+			</div>
+		</form>
+	</div>
+	<script type='text/javascript' src='//s3.amazonaws.com/downloads.mailchimp.com/js/mc-validate.js'></script>
+	<script type='text/javascript'>(function ($) {
+			window.fnames = new Array();
+			window.ftypes = new Array();
+			fnames[0] = 'EMAIL';
+			ftypes[0] = 'email';
+		}(jQuery));
+		var $mcj = jQuery.noConflict(true);</script>
+	<!--End mc_embed_signup-->
+	<?php
+}
