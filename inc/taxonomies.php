5cee5f237cec7f414bd32e7d8b9c26eec2dcc48e
Michael Beckwith <tw2113@gmail.com>
2017-04-01 21:26:27 -0500

    localize some taxonomy data for dynamically checking while the user types in a slug

--- inc/taxonomies.php
+++ inc/taxonomies.php
@@ -33,10 +33,16 @@ function cptui_taxonomies_enqueue_scripts() {
 	}
 
 	wp_enqueue_script( 'cptui' );
+
+	$core    = get_taxonomies( array( '_builtin' => true ) );
+	$public  = get_taxonomies( array( '_builtin' => false, 'public' => true ) );
+	$private = get_taxonomies( array( '_builtin' => false, 'public' => false ) );
+	$registered_taxonomies = array_merge( $core, $public, $private );
 	wp_localize_script( 'cptui', 'cptui_tax_data',
 		array(
 			'confirm'            => esc_html__( 'Are you sure you want to delete this? Deleting will NOT remove created content.', 'custom-post-type-ui' ),
 			'no_associated_type' => esc_html( 'Please select a post type to associate with.', 'custom-post-type-ui' ),
+			'existing_taxonomies' => $registered_taxonomies,
 		)
 	);
 }
