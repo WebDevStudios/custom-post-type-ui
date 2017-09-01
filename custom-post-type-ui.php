8ad96af9061ee839ec3e39830e6e596417fc7d26
Michael Beckwith <tw2113@gmail.com>
2017-09-01 14:22:10 -0500

    attempt to prevent undefined index notices for has_archive

--- custom-post-type-ui.php
+++ custom-post-type-ui.php
@@ -352,7 +352,7 @@ function cptui_register_single_post_type( $post_type = array() ) {
 		}
 	}
 
-	$has_archive = get_disp_boolean( $post_type['has_archive'] );
+	$has_archive = ( isset( $post_type['has_archive'] ) ) ? get_disp_boolean( $post_type['has_archive'] ) : false;
 	if ( ! empty( $post_type['has_archive_string'] ) ) {
 		$has_archive = $post_type['has_archive_string'];
 	}
