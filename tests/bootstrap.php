34c829db72e92a2010c592b810b78344f75c78c9
Michael Beckwith <tw2113@gmail.com>
2017-11-19 23:06:23 -0600

    a bit of a mess, but begin decoupling from the WP integration test setup

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -3,6 +3,8 @@
 ini_set('display_errors','on');
 error_reporting(E_ALL);
 
+use Brain\Monkey;
+
 /**
  * Set `WP_TESTS_DIR` to the base directory of WordPress:
  * `svn export http://develop.svn.wordpress.org/trunk/ /tmp/wordpress-tests`
@@ -11,19 +13,19 @@ error_reporting(E_ALL);
  *
  * export WP_TESTS_DIR=/tmp/wordpress/tests
  */
-if ( ! $wp_test_dir = getenv('WP_TESTS_DIR') ) {
+/*if ( ! $wp_test_dir = getenv('WP_TESTS_DIR') ) {
 
 	$wp_test_dir = '/tmp/wordpress-tests-lib';
 
 	if ( ! file_exists( $wp_test_dir . '/includes' ) ) {
 		die( "Fatal Error: Could not find the WordPress tests directory.\n" );
 	}
-}
+}*/
 
 /**
  * Loads WP utility functions like `tests_add_filter` and `_delete_all_posts`.
  */
-require_once $wp_test_dir . '/includes/functions.php';
+#require_once $wp_test_dir . '/includes/functions.php';
 
 /**
  * Preset wp_options before loading the WordPress stack.
@@ -32,23 +34,24 @@ require_once $wp_test_dir . '/includes/functions.php';
  *
  * @see wp_tests_options
  */
-$GLOBALS['wp_tests_options'] = array(
+/*$GLOBALS['wp_tests_options'] = array(
 	'active_plugins' => array(
 		'hello.php',
 	),
-);
+);*/
 
 /**
  * Run custom functionality after mu-plugins are loaded.
  */
-function _tests_load_cptui() {
-	define( 'CPTUI_DIRECTORY_PATH', trailingslashit( dirname( dirname( __FILE__ ) ) ) );
+#function _tests_load_cptui() {
+	define( 'CPTUI_DIRECTORY_PATH', dirname( dirname( __FILE__ ) ) . '/' );
 	require CPTUI_DIRECTORY_PATH . '/vendor/autoload.php';
-	require CPTUI_DIRECTORY_PATH . 'custom-post-type-ui.php';
-}
-tests_add_filter( 'muplugins_loaded', '_tests_load_cptui' );
+	require CPTUI_DIRECTORY_PATH . '/classes/class.cptui_admin_ui.php';
+	#require CPTUI_DIRECTORY_PATH . 'custom-post-type-ui.php';
+#}
+#tests_add_filter( 'muplugins_loaded', '_tests_load_cptui' );
 
 /**
  * Bootstraps the WordPress stack.
  */
-require $wp_test_dir . '/includes/bootstrap.php';
+#require $wp_test_dir . '/includes/bootstrap.php';
