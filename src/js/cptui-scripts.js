5abc3e1868033d6f0ba7ce9ab1bd9a5c0f78e0cc
Michael Beckwith <tw2113@gmail.com>
2024-01-24 09:23:26 -0600

    utilize constants in our getParameterByName function

--- src/js/cptui-scripts.js
+++ src/js/cptui-scripts.js
@@ -196,7 +196,7 @@ postboxes.add_postbox_toggles(pagenow);
 	function getParameterByName(name, url) {
 		if (!url) url = window.location.href;
 		name = name.replace(/[\[\]]/g, "\\$&");
-		var regex = new RegExp("[?&]" + name + "(=([^&#]*)|&|#|$)"),
+		const regex = new RegExp("[?&]" + name + "(=([^&#]*)|&|#|$)"),
 			results = regex.exec(url);
 		if (!results) return null;
 		if (!results[2]) return '';
