feae468d57dfa6a0a8549d76780317d84a1d38e0
Michael Beckwith <tw2113@gmail.com>
2024-03-12 11:19:33 -0500

    make sure we have our nameField before attempting to work with it

--- src/js/cptui-scripts.js
+++ src/js/cptui-scripts.js
@@ -165,51 +165,53 @@ postboxes.add_postbox_toggles(pagenow);
 		});
 	});
 
-	// Switch spaces for underscores on our slug fields.
-	nameField.addEventListener('keyup', (e) => {
-		let value, original_value;
+	if (nameField) {
+		// Switch spaces for underscores on our slug fields.
+		nameField.addEventListener('keyup', (e) => {
+			let value, original_value;
 
-		value = original_value = e.currentTarget.value;
-		let keys = ['Tab', 'ArrowLeft', 'ArrowUp', 'ArrowRight', 'ArrowDown'];
-		if (!keys.includes(e.code)) {
-			value = value.replace(/ /g, "_");
-			value = value.toLowerCase();
-			value = replaceDiacritics(value);
-			value = transliterate(value);
-			value = replaceSpecialCharacters(value);
-			if (value !== original_value) {
-				e.currentTarget.value = value;
+			value = original_value = e.currentTarget.value;
+			let keys = ['Tab', 'ArrowLeft', 'ArrowUp', 'ArrowRight', 'ArrowDown'];
+			if (!keys.includes(e.code)) {
+				value = value.replace(/ /g, "_");
+				value = value.toLowerCase();
+				value = replaceDiacritics(value);
+				value = transliterate(value);
+				value = replaceSpecialCharacters(value);
+				if (value !== original_value) {
+					e.currentTarget.value = value;
+				}
 			}
-		}
 
-		//Displays a message if slug changes.
-		if (typeof original_slug !== 'undefined') {
-			let slugchanged = document.querySelector('#slugchanged');
-			if (value !== original_slug) {
-				slugchanged.classList.remove('hidemessage');
-			} else {
-				slugchanged.classList.add('hidemessage');
+			//Displays a message if slug changes.
+			if (typeof original_slug !== 'undefined') {
+				let slugchanged = document.querySelector('#slugchanged');
+				if (value !== original_slug) {
+					slugchanged.classList.remove('hidemessage');
+				} else {
+					slugchanged.classList.add('hidemessage');
+				}
 			}
-		}
 
-		let slugexists = document.querySelector('#slugexists');
-		let override = document.querySelector('#override_validation');
-		let override_validation = (override) ? override.check : false;
-		if (typeof cptui_type_data != 'undefined') {
-			if (cptui_type_data.existing_post_types.hasOwnProperty(value) && value !== original_slug && override_validation === false) {
-				slugexists.classList.remove('hidemessage');
-			} else {
-				slugexists.classList.add('hidemessage');
+			let slugexists = document.querySelector('#slugexists');
+			let override = document.querySelector('#override_validation');
+			let override_validation = (override) ? override.check : false;
+			if (typeof cptui_type_data != 'undefined') {
+				if (cptui_type_data.existing_post_types.hasOwnProperty(value) && value !== original_slug && override_validation === false) {
+					slugexists.classList.remove('hidemessage');
+				} else {
+					slugexists.classList.add('hidemessage');
+				}
 			}
-		}
-		if (typeof cptui_tax_data != 'undefined') {
-			if (cptui_tax_data.existing_taxonomies.hasOwnProperty(value) && value !== original_slug) {
-				slugexists.classList.remove('hidemessage');
-			} else {
-				slugexists.classList.add('hidemessage');
+			if (typeof cptui_tax_data != 'undefined') {
+				if (cptui_tax_data.existing_taxonomies.hasOwnProperty(value) && value !== original_slug) {
+					slugexists.classList.remove('hidemessage');
+				} else {
+					slugexists.classList.add('hidemessage');
+				}
 			}
-		}
-	});
+		});
+	}
 
 	// Replace diacritic characters with latin characters.
 	function replaceDiacritics(s) {
