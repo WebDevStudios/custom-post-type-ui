a5c8459f8269d5cb57015f1c1304eb591867aeb6
Michael Beckwith <tw2113@gmail.com>
2024-04-17 15:45:53 -0500

    revise entry point

--- src/js/cptui.js
+++ src/js/cptui.js
@@ -1,11 +1,14 @@
 import './partials/utils';
+import './partials/hide-submit';
 import './partials/toggle-hierarchical';
 import './partials/autoswitch';
+import './partials/confirm-delete';
 import './partials/support-toggles';
+import './partials/namefield';
+import './partials/menu-icon';
 import './partials/tax-required-post-type';
 import './partials/autopopulate';
 import './partials/back-to-top';
 import './partials/toggle-panels';
 
-import './cptui-scripts';
-import './dashicons-picker';
+//import './dashicons-picker';
