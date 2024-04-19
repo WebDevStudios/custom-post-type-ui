e119133b9728cd8dfdeb03166ed008a0876b95a5
Michael Beckwith <tw2113@gmail.com>
2024-04-19 13:55:02 -0500

    webpack updates for not minimizing

--- webpack.config.js
+++ webpack.config.js
@@ -3,7 +3,11 @@ const defaultConfig = require('@wordpress/scripts/config/webpack.config');
 module.exports = {
 	...defaultConfig,
 	entry: {
-		cptui: './src/js/cptui.js',
-		dashiconsPicker: './src/js/dashicons-picker'
-	}
+		"cptui": './src/js/cptui.js',
+		"dashiconsPicker": './src/js/dashiconsPicker'
+	},
+	optimization: {
+		minimize: false
+	},
+	devtool: 'source-map',
 };
