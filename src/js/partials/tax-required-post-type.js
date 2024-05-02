bcac8e5ad7e5312ac679a0226c27dc6e0e7d66c0
Michael Beckwith <tw2113@gmail.com>
2024-05-02 15:01:03 -0500

    file purpose description for taxonomies needing a post type dialog

--- src/js/partials/tax-required-post-type.js
+++ src/js/partials/tax-required-post-type.js
@@ -1,5 +1,13 @@
 'use strict';
 
+/*
+ * This file provides a dialog box to alert the user that at least one post type must be chosen
+ * before they can save a taxonomy.
+ *
+ * This was added because taxonomies need to have a post type, meanwhile post types do NOT need
+ * to have a taxonomy.
+ */
+
 (() => {
 	// Handles checking if a post type has been chosen or not when adding/saving a taxonomy.
 	// Post type associations are a required attribute.
