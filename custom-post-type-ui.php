c985e2f71f7c794f96bf1a9af7262e59a12e5981
Michael Beckwith <tw2113@gmail.com>
2025-12-17 16:45:29 -0600

    pass single labels through WPML admin texts to better integrate with their plugins

--- custom-post-type-ui.php
+++ custom-post-type-ui.php
@@ -405,6 +405,9 @@ function cptui_register_single_post_type( array $post_type = [] ) {
 	$preserved_labels = cptui_get_preserved_labels();
 	foreach ( $post_type['labels'] as $key => $label ) {
 
+		$text_name = "[cptui_post_types][{$post_type['name']}][labels]{$key}";
+		$label     = apply_filters( 'wpml_translate_single_string', $label, 'admin_texts_cptui_post_types', $text_name );
+
 		if ( ! empty( $label ) ) {
 			if ( 'parent' === $key ) {
 				$labels['parent_item_colon'] = $label;
