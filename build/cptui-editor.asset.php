2ace4ef29d4a4d4a8ad2cf24d3c6ab91a146d080
Michael Beckwith <tw2113@gmail.com>
2026-05-07 09:29:36 -0500

    remove build folder from vcs

--- build/cptui-editor.asset.php
+++
@@ -1,1 +0,0 @@
-<?php return array('dependencies' => array('react-jsx-runtime', 'wp-api-fetch', 'wp-components', 'wp-data', 'wp-editor', 'wp-element', 'wp-i18n', 'wp-plugins'), 'version' => '229f60f358863168d123');
--- build/cptui.asset.php
+++
@@ -1,1 +0,0 @@
-<?php return array('dependencies' => array(), 'version' => 'ebcb808cd423da899449');
