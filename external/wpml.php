50566d331007c9e05ccdbe2bb883e43d9872bf8d
Michael Beckwith <tw2113@gmail.com>
2026-07-10 09:15:20 -0500

    fill in phpdocs

--- external/wpml.php
+++ external/wpml.php
@@ -4,6 +4,14 @@ if ( ! defined( 'ABSPATH' ) ) {
 	exit;
 }
 
+/**
+ * Builds a WPML package for a post type.
+ *
+ * @since 1.19.3
+ *
+ * @param string $slug Slug to register package for.
+ * @return array
+ */
 function cptui_wpml_pkg_build_post_type_package( $slug ) {
 	return [
 		'kind'      => 'CPTUI Post Type',
@@ -18,6 +26,14 @@ function cptui_wpml_pkg_build_post_type_package( $slug ) {
 	];
 }
 
+/**
+ * Builds a WPML package fpr a taxonomy.
+ *
+ * @since 1.19.3
+ *
+ * @param string $slug Slug to register package for.
+ * @return array
+ */
 function cptui_wpml_pkg_build_taxonomy_package( $slug ) {
 	return [
 		'kind'      => 'CPTUI Taxonomy',
@@ -32,6 +48,14 @@ function cptui_wpml_pkg_build_taxonomy_package( $slug ) {
 	];
 }
 
+/**
+ * Register string package kinds for CPTUI.
+ *
+ * @since 1.19.3
+ *
+ * @param $kinds
+ * @return mixed
+ */
 function cptui_register_string_package_kinds( $kinds ) {
 	$kinds['cptui-post-type'] = esc_html__( 'CPT UI Post Type', 'custom-post-type-ui' );
 	$kinds['cptui-taxonomy']  = esc_html__( 'CPT UI Taxonomy', 'custom-post-type-ui' );
@@ -40,6 +64,13 @@ function cptui_register_string_package_kinds( $kinds ) {
 }
 add_filter( 'wpml_active_string_package_kinds', 'cptui_register_string_package_kinds' );
 
+/**
+ * Register and save translateable content to WPML after settings save of post type.
+ *
+ * @since 1.19.3
+ *
+ * @param array $data Array of post type data.
+ */
 function cptui_register_post_type_string_packages( $data ) {
 	$package = cptui_wpml_pkg_build_post_type_package( $data['name'] );
 	do_action( 'wpml_start_string_package_registration', $package );
@@ -61,6 +92,13 @@ function cptui_register_post_type_string_packages( $data ) {
 }
 add_action( 'cptui_after_update_post_type', 'cptui_register_post_type_string_packages' );
 
+/**
+ * Register and save translateble content to WPML after settings save of taxonomy.
+ *
+ * @since 1.19.3
+ *
+ * @param array $data Array of taxonomy data.
+ */
 function cptui_register_taxonomy_string_packages( $data ) {
 	$package = cptui_wpml_pkg_build_taxonomy_package( $data['name'] );
 	do_action( 'wpml_start_string_package_registration', $package );
@@ -82,15 +120,41 @@ function cptui_register_taxonomy_string_packages( $data ) {
 }
 add_action( 'cptui_after_update_taxonomy', 'cptui_register_taxonomy_string_packages' );
 
+/**
+ * Remove post type package after deletion of post type.
+ *
+ * @since 1.19.3
+ *
+ * @param array $data Post type data.
+ */
 function cptui_delete_post_type_string_packages( $data ) {
 	do_action( 'wpml_delete_package', $data['name'], 'CPTUI Post Type' );
 }
 add_action( 'cptui_after_delete_post_type', 'cptui_delete_post_type_string_packages' );
+
+/**
+ * Remove taxonomy package after deletion of taxonomy.
+ *
+ * @since 1.19.3
+ *
+ * @param array $data Taxonomy data.
+ */
 function cptui_delete_taxonomy_string_packages( $data ) {
 	do_action( 'wpml_delete_package', $data['name'], 'CPTUI Taxonomy' );
 }
 add_action( 'cptui_after_delete_taxonomy', 'cptui_delete_taxonomy_string_packages' );
 
+/**
+ * Filter in translated strings before post type registration.
+ *
+ * @since 1.19.3
+ *
+ * @param array  $args      Post type registration args.
+ * @param string $slug      Post type slug being registered.
+ * @param array  $post_type CPTUI individual post type settings.
+ *
+ * @return array
+ */
 function cptui_wpml_apply_post_type_translations( $args, $slug, $post_type ) {
 	$package = cptui_wpml_pkg_build_post_type_package( $slug );
 	if ( ! empty( $args['labels'] ) && is_array( $args['labels'] ) ) {
@@ -109,6 +173,17 @@ function cptui_wpml_apply_post_type_translations( $args, $slug, $post_type ) {
 }
 add_filter( 'cptui_pre_register_post_type', 'cptui_wpml_apply_post_type_translations', 20, 3 );
 
+/**
+ * Filter in translated strings before taxonomy registration.
+ *
+ * @since 1.19.3
+ *
+ * @param array  $args     Taxonomy registration args.
+ * @param string $slug     Taxonomy slug being registered.
+ * @param array  $taxonomy CPTUI individual taxonomy setings.
+ *
+ * @return array
+ */
 function cptui_wpml_apply_taxonomy_translations( $args, $slug, $taxonomy ) {
 	$package = cptui_wpml_pkg_build_taxonomy_package( $slug );
 	if ( ! empty( $args['labels'] ) && is_array( $args['labels'] ) ) {
